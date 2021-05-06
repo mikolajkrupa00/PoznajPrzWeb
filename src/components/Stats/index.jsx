d8a406037aaaed6eadc11afdbbcdd715bd2d1a26
mateuszkochmanski <82410139+mateuszkochmanski@users.noreply.github.com>
2021-05-06 15:33:25 +0200

    Update StatsPage

--- src/components/Stats/index.jsx
+++ src/components/Stats/index.jsx
@@ -5,13 +5,13 @@ import componentStyles from "./styles";
 
 const StatsPage = () => {
     const[visits, setVisits] = useState()
+    const [ratings, setRatings] = useState()
     const[expanded, seExpanded] = useState()
     const { PlacesContainer, Place, PlaceName, PlaceAddress, PlaceCategory, PlaceNumOfVisits, PlaceDesc, PlaceImg, PlaceId} = componentStyles;
     useEffect(() => {
-        Axios.get("/visit/getStats/365").then(res => setVisits(res.data))
+        Axios.get("/visit/getStats/365").then(res => setVisits(res.data));
+        Axios.get(`/Place/getPlaces/1c45f396-84f8-49cb-9d1b-849de68cdd0d`).then(res => setRatings(res.data));
     },[])
-
-
 
     return(
         <Layout>
@@ -25,6 +25,7 @@ const StatsPage = () => {
                             <PlaceAddress>{visit.address}</PlaceAddress>
                             <PlaceCategory>kategoria :  {visit.categoryName}</PlaceCategory>
                             <PlaceNumOfVisits>liczba odwiedzin w ostatnich 365 dniach:  {visit.numOfVisits}</PlaceNumOfVisits>
+                            {console.log(ratings)}
                         </PlaceDesc>
                     </Place>
                 )}
