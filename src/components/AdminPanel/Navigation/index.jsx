c0e3889557b8bc0465644a9398ae3f0fac9abe9e
Rafal <rafii2000@interia.pl>
2021-06-03 14:45:45 +0200

    Improve Navigation section's styles

--- src/components/AdminPanel/Navigation/index.jsx
+++ src/components/AdminPanel/Navigation/index.jsx
@@ -1,18 +1,65 @@
 import components from "./styles";
-import { NavLink } from 'react-router-dom';
+import { useHistory, NavLink } from 'react-router-dom';
 import block_user from '../../../assets/admin_panel_icons/block-user.png';
 import confirm_place from '../../../assets/admin_panel_icons/signal.png';
 import stats from '../../../assets/admin_panel_icons/line-graph.png';
 
 const Navigation = () => {
-    const {Menu,Img,Icon} = components;
+    const {Menu,Img,Icon, LinkLabel} = components;
+    const history = useHistory();
+
+    //history.push("home");
+
+    const NavLinkStyles = {
+
+        width: "75px",
+        margin: "0px 5px",
+        display: "flex",
+        flexDirection : "column",
+        justifyContent: "center",
+        alignContent: "center",
+        justifyItems: "center",
+        alignItems: "center",
+        borderBottom:"solid 2px whitesmoke",
+        paddingBottom: "3px",
+        whiteSpace: "normal",
+        }
+
     return ( 
         <Menu>
-                <NavLink to="/adminPanel/" exact activeStyle={{borderBottom:"solid 2px gray"}}><Icon>1</Icon></NavLink>
-                <NavLink to="/adminPanel/statistics" activeStyle={{borderBottom:"solid 2px gray"}}><Icon><Img src={stats} alt="Statystyki"/></Icon></NavLink>
-                <NavLink to="/adminPanel/blockedUsers" activeStyle={{borderBottom:"solid 2px gray"}}><Icon><Img src={block_user} alt="Zablokowani użytkownicy"/></Icon></NavLink>
-                <NavLink to="/adminPanel/confirmPlace" activeStyle={{borderBottom:"solid 2px gray"}}><Icon><Img src={confirm_place} alt=''/></Icon></NavLink>
+                <NavLink to="/adminPanel/" exact activeStyle={{borderBottom:"solid 2px gray"}} style={NavLinkStyles}>
+                    <Icon><Img src={confirm_place} alt=''/></Icon>
+                    <LinkLabel>Strona główna</LinkLabel>                                 
+                </NavLink>
+
+                <NavLink to="/adminPanel/statistics" activeStyle={{borderBottom:"solid 2px gray"}} style={NavLinkStyles}>
+                    <Icon><Img src={stats} alt="Statystyki"/></Icon>
+                    <LinkLabel>Statystyki</LinkLabel>
+                </NavLink>
+                
+                <NavLink to="/adminPanel/blockedUsers" activeStyle={{borderBottom:"solid 2px gray"}} style={NavLinkStyles}>
+                    <Icon><Img src={block_user} alt="Zablokowani użytkownicy"/></Icon>
+                    <LinkLabel>Zablokowani użytkownicy</LinkLabel>
+
+                </NavLink>
+                
+                <NavLink to="/adminPanel/confirmPlace" activeStyle={{borderBottom:"solid 2px gray"}} style={NavLinkStyles}>
+                    <Icon><Img src={confirm_place} alt=''/></Icon>
+                    <LinkLabel>Propozycje miejsc</LinkLabel>                   
+                </NavLink>
+
+
+                <NavLink to="/adminPanel/path_to_sth_1" activeStyle={{borderBottom:"solid 2px gray"}} style={NavLinkStyles}>
+                    <Icon><Img src={confirm_place} alt=''/></Icon>
+                    <LinkLabel>Nazwa Sekcji 1</LinkLabel>                   
+                </NavLink>       
+
+                <NavLink to="/adminPanel/path_to_sth_2" activeStyle={{borderBottom:"solid 2px gray"}} style={NavLinkStyles}>
+                    <Icon><Img src={confirm_place} alt=''/></Icon>
+                    <LinkLabel>Nazwa Sekcji 2</LinkLabel>                   
+                </NavLink>       
         </Menu>
+
      )
 }
  
--- src/components/AdminPanel/Navigation/styles.js
+++ src/components/AdminPanel/Navigation/styles.js
@@ -2,20 +2,36 @@ import styled from "styled-components"
 
 const componentStyles = {
     
-    Menu :  styled.nav`
-    display:flex;
+    Menu:  styled.nav`
+    display: -webkit-box;
     margin: 30px 0px;
-    justify-content: space-around;
+    margin-bottom: 100px;
     width: 100%;
-    height: 50px;
+    justify-content: space-around;
+    overflow-y: scroll;
+    -ms-overflow-style: none;   /* IE and Edge */
+    scrollbar-width: none;      /* Firefox */
+
+    &::-webkit-scrollbar{
+        display: none;
+    }
     `,
+
     Icon : styled.div`
     width: 50px;
     padding: 5px;
     `,
+    
     Img: styled.img`
     width: 20px;
-    
-    `
+    `,
+
+    LinkLabel: styled.div`
+    font-size: 12px;
+    height: 30px;
+    line-height: 15px;
+    margin-top: 5px;
+    `,
+
 }
 export default componentStyles;
