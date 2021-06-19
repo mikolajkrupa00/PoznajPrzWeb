25e6d0a4bd6200984f371d3a9629a14512c7a75e
mateuszkochmanski <82410139+mateuszkochmanski@users.noreply.github.com>
2021-06-19 18:49:44 +0200

    BlockUser - validation

--- src/components/AdminPanel/BlockUser/index.jsx
+++ src/components/AdminPanel/BlockUser/index.jsx
@@ -7,6 +7,8 @@ import {useState, useEffect} from "react";
 const BlockUser = () => {
     const {register, handleSubmit} = useForm();
     const [blockedUsers,setBlockedUsers] = useState();
+    const [blockError, setBlockError] = useState("")
+    const [unBlockMessage, setUnblockMassage] = useState("")
     const {BlockUserSubmit, BlockUserInput, BlockUserContainer, BlockedUsers, User, UserName, UserEmail, UnlockUserSubmit} = components;
 
     useEffect(() => {
@@ -17,22 +19,34 @@ const BlockUser = () => {
     }
 
     const blockUser = (data) =>{
-        Axios.put(`/user/blockUser/${data.username}`).then(getUnblockedUsers);
+        Axios.put(`/user/blockUser/${data.username}`).then((res)=>{
+            if (res.status === 200 && res.data) {
+                setBlockError(1)
+            } 
+            else {
+                setBlockError(2)
+            }
+        }).catch(() => {
+            setBlockError(2)
+        }).then(getUnblockedUsers);
     }
     const unblockUser = (username) =>{
         Axios.put(`/user/unblockUser/${username}`).then(getUnblockedUsers)
-        
+        setUnblockMassage(`Użytkownik ${username} został odblokowany`);
     }
     return ( 
         <>
             <BlockUserContainer>
                         <BlockUserInput type="text" {...register('username', {required:true})}/>
                         <BlockUserSubmit onClick={handleSubmit(data => blockUser(data))}>Zablokuj</BlockUserSubmit>
+                        {blockError===1 ? <p style={{color:'green',paddingTop:'15px'}}>Użytkownik zablokowany</p> : null}
+                        {blockError===2 ? <p style={{color:'firebrick',paddingTop:'15px'}}>Użytkownik nie istnieje lub został już zablokowany</p> : null}
             </BlockUserContainer>
             <BlockedUsers>
                 <h3>Zablokowani użytkownicy</h3>
+                {unBlockMessage && <p style={{color:'green',paddingTop:'15px'}}>{unBlockMessage}</p>}
                 {blockedUsers && blockedUsers.map(user=>
-                    <User key={`key${user.userid}`}>
+                    <User key={`key${user.userId}`}>
                         <UserName>Nazwa użytkownika: {user.username}</UserName>
                         <UserEmail>Email: {user.email}</UserEmail>
                         <UnlockUserSubmit onClick={()=>unblockUser(user.username)}>Odblokuj użytkownika</UnlockUserSubmit>
